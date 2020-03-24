867daacc02ff80f0755358ae53ecdedf2f0bbef1
Anna <s16782@pjwstk.edu.pl>
2020-03-24 22:06:49 +0100

    zadanko cebula

---
+++ Onion.Infrastructure.MockDbService/Services/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Onion.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Onion.Infrastructure.MockDbService.Services
+{
+    public class StudentDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=db-mssql;Initial Catalog=s16782;Integrated Security=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ Onion.Infrastructure.MockDbService/Services/StudentDbService.cs
@@ -0,0 +1,28 @@
+﻿using Onion.Domain.Entities;
+using Onion.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Onion.Infrastructure.MockDbService.Services
+{
+    public class StudentDbService : IStudentDbService
+    {
+        private StudentDbContext _context = new StudentDbContext();
+        public StudentDbService()
+        {
+          
+        }
+        public bool EnrollStudent(Student newStudent, int semestr)
+        {
+            _context.Student.Add(newStudent);
+            return true;
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _context.Student.ToList();
+        }
+    }
+}
