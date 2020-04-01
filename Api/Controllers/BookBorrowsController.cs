803a62621ee7de48c527511668255cde906f2f2d
Anna <s16782@pjwstk.edu.pl>
2020-04-01 19:00:37 +0200

    logowanie_bledow

---
+++ Api/Controllers/BookBorrowsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Library.Models.DTO;
+using Library.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLog;
+using Rollbar;
+
+namespace Library.Controllers
+{
+    [Route("api/book-borrows")]
+    //[Authorize(Roles="Reader")]
+    public class BookBorrowsController : Controller
+    {
+        private readonly IBookBorrowRepository _bookBorrowRepository;
+        private readonly ILogger<BookBorrowsController> _logger;
+
+        public BookBorrowsController(IBookBorrowRepository bookBorrowRepository, ILogger<BookBorrowsController> logger)
+        {
+            _bookBorrowRepository = bookBorrowRepository;
+            _logger = logger;
+        }
+
+
+        [HttpPost(Name = nameof(AddBookBorrow))]
+        public async Task<IActionResult> AddBookBorrow([FromBody] BookBorrowDto borrow)
+        {
+            RollbarLocator.RollbarInstance.Error(new Exception("Błąd z Rollbar metoda addBookBorrow"));
+            _logger.LogError("Wystąpił błąd przy dodawaniu ksiazki");
+
+            var res = await _bookBorrowRepository.AddBookBorrow(borrow);
+            return CreatedAtRoute(nameof(AddBookBorrow), res);
+        }
+
+        [HttpPut("{idBookBorrow}")]
+        public async Task<IActionResult> UpdateBookBorrow([FromBody] UpdateBookBorrowDto borrow)
+        {
+            RollbarLocator.RollbarInstance.Error(new Exception("Błąd z Rollbar metoda updateBookBorrow"));
+            _logger.LogError("Wystąpił błąd przy dodawaniu ksiazki");
+
+            await _bookBorrowRepository.ChangeBookBorrow(borrow);
+            return NoContent();
+        }
+
+        
+    }
+}
---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Library.Models.DTO;
+using Library.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Library.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(IUserRepository userRepository, ILogger<UsersController> logger)
+        {
+            _userRepository = userRepository;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var res = await _userRepository.GetUsers();
+            return Ok(res);
+        }
+
+        [HttpPost(Name = nameof(AddUser))]
+        public async Task<IActionResult> AddUser([FromBody] UserDto user)
+        {
+            RollbarLocator.RollbarInstance.Error(new Exception("Błąd z Rollbar metoda addUser"));
+            _logger.LogError("Wystąpił błąd przy dodawaniu ksiazki");
+
+            var res = await _userRepository.AddUser(user);
+
+            return CreatedAtRoute(nameof(AddUser), res);
+        }
+
+        [HttpGet("{idUser:int}")]
+        public async Task<IActionResult> GetUser([FromRoute] int idUser)
+        {
+            RollbarLocator.RollbarInstance.Error(new Exception("Błąd z Rollbar metoda GetUser"));
+            _logger.LogError("Wystąpił błąd przy dodawaniu ksiazki");
+
+            var res = await _userRepository.GetUser(idUser);
+            return Ok(res);
+        }
+        
+    }
+}
